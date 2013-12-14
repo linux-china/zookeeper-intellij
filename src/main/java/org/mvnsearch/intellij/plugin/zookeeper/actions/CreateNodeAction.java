d9345bc4c1873d15619af95dc37c42785d8cd9bf
Jacky Chan <libing.chen@gmail.com>
2013-12-14 21:49:33 +0800

    implement create logic

--- src/main/java/org/mvnsearch/intellij/plugin/zookeeper/actions/CreateNodeAction.java
+++ src/main/java/org/mvnsearch/intellij/plugin/zookeeper/actions/CreateNodeAction.java
@@ -2,9 +2,15 @@ package org.mvnsearch.intellij.plugin.zookeeper.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.DialogBuilder;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.treeStructure.Tree;
+import org.apache.curator.framework.CuratorFramework;
+import org.jetbrains.generate.tostring.util.StringUtil;
 import org.mvnsearch.intellij.plugin.zookeeper.ZkProjectComponent;
+import org.mvnsearch.intellij.plugin.zookeeper.ui.ZkNode;
 
+import javax.swing.*;
 import javax.swing.tree.TreePath;
 
 /**
@@ -13,8 +19,29 @@ import javax.swing.tree.TreePath;
  * @author linux_china
  */
 public class CreateNodeAction extends AnAction {
-    public void actionPerformed(AnActionEvent anActionEvent) {
-        Tree zkTree = ZkProjectComponent.getInstance(anActionEvent.getProject()).getZkTree();
-        TreePath treePath = zkTree.getSelectionPath();
+    public void actionPerformed(final AnActionEvent anActionEvent) {
+        final DialogBuilder builder = new DialogBuilder(anActionEvent.getProject());
+        builder.setTitle("Create Node");
+        final JTextField jTextField = new JTextField();
+        builder.setCenterPanel(jTextField);
+        builder.setOkOperation(new Runnable() {
+            public void run() {
+                String nodeName = jTextField.getText();
+                ZkProjectComponent zkProjectComponent = ZkProjectComponent.getInstance(anActionEvent.getProject());
+                if (StringUtil.isNotEmpty(jTextField.getText())) {
+                    Tree zkTree = zkProjectComponent.getZkTree();
+                    TreePath treePath = zkTree.getSelectionPath();
+                    CuratorFramework curator = zkProjectComponent.getCurator();
+                    ZkNode currentNode = (ZkNode) treePath.getLastPathComponent();
+                    try {
+                        curator.create().forPath(currentNode.getSubNode(nodeName).getFilePath(), new byte[0]);
+                    } catch (Exception ignore) {
+                        ignore.printStackTrace();
+                    }
+                    builder.getDialogWrapper().close(DialogWrapper.OK_EXIT_CODE);
+                }
+            }
+        });
+        builder.showModal(true);
     }
 }
