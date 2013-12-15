2318c8534e03094deeb2b9089256cfecdfe600c9
Jacky Chan <libing.chen@gmail.com>
2013-12-15 20:03:27 +0800

    Refresh Zoo Keeper Tree

---
+++ src/main/java/org/mvnsearch/intellij/plugin/zookeeper/actions/ZkTreeRefreshAction.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.intellij.plugin.zookeeper.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.mvnsearch.intellij.plugin.zookeeper.ZkProjectComponent;
+
+/**
+ * Refresh Zoo Keeper Tree
+ *
+ * @author linux_china
+ */
+public class ZkTreeRefreshAction extends AnAction {
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        ZkProjectComponent zkProjectComponent = ZkProjectComponent.getInstance(anActionEvent.getProject());
+        zkProjectComponent.reloadZkTree();
+    }
+}
